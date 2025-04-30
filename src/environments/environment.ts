b75160b3dd0d2f28b4ac016b59f3d6f95cafad53
siddhi.shinde <siddhi.shnde@tekditechnologies.com>
2025-04-30 17:26:21 +0530

    Changed in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,15 @@ interface Environment {
   baseURL: string;
   production: boolean;
   surveyBaseURL?: string;
-  projectsBaseURL?:string;
-  capabilities:'all' | 'project' | 'survey';
-  restrictedPages: any,
-  unauthorizedRedirectUrl:string
-  isAuthBypassed: any,
-  config:any,
-  profileRedirectPath: any,
-  showHeader:any
+  projectsBaseURL?: string;
+  capabilities: 'all' | 'project' | 'survey';
+  restrictedPages: any;
+  unauthorizedRedirectUrl: string;
+  isAuthBypassed: any;
+  config: any;
+  profileRedirectPath: any;
+  showHeader: any;
+  contentPolicyURL: string;
 }
 
 //projects and survey for non-docker
@@ -21,19 +22,32 @@ interface Environment {
 // }s
 
 //projects and survey for docker
-export const environment:Environment = {
+export const environment: Environment = {
   production: true,
   baseURL: window['env' as any]['baseURL' as any] as unknown as string,
-  projectsBaseURL: window['env' as any]['projectsBaseURL' as any] as unknown as string,
-  surveyBaseURL: window['env' as any]['surveyBaseURL' as any] as unknown as string,
-  capabilities:window['env' as any]['capabilities' as any] as unknown as any,
+  projectsBaseURL: window['env' as any][
+    'projectsBaseURL' as any
+  ] as unknown as string,
+  surveyBaseURL: window['env' as any][
+    'surveyBaseURL' as any
+  ] as unknown as string,
+  capabilities: window['env' as any]['capabilities' as any] as unknown as any,
   restrictedPages: window['env' as any]['restrictedPages' as any],
-  unauthorizedRedirectUrl: window['env' as any]['unauthorizedRedirectUrl' as any] as unknown as string,
-  isAuthBypassed: window['env' as any]['isAuthBypassed' as any] as unknown as any,
+  unauthorizedRedirectUrl: window['env' as any][
+    'unauthorizedRedirectUrl' as any
+  ] as unknown as string,
+  isAuthBypassed: window['env' as any][
+    'isAuthBypassed' as any
+  ] as unknown as any,
   config: window['env' as any]['config' as any] as unknown as any,
-  profileRedirectPath: window['env' as any]['profileRedirectPath' as any] as unknown as any,
+  profileRedirectPath: window['env' as any][
+    'profileRedirectPath' as any
+  ] as unknown as any,
   showHeader: window['env' as any]['showHeader' as any] as unknown as any,
-}
+  contentPolicyURL: window['env' as any][
+    'contentPolicyURL' as any
+  ] as unknown as any,
+};
 
 //survey-only
 
