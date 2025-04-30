a5b1adc3f10a290c2f969a7b66490250fc5d641a
siddhi.shinde <siddhi.shnde@tekditechnologies.com>
2025-04-30 10:08:05 +0530

    Removed routing

--- src/app/services/guard/allowPageAccess/allow-page-access.guard.ts
+++ src/app/services/guard/allowPageAccess/allow-page-access.guard.ts
@@ -1,14 +1,26 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-export const allowPageAccessGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): 
-Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree  => {
-  const router = inject(Router)
-  if(environment.restrictedPages.includes(route.data["pageId"])){
-    location.href = environment.unauthorizedRedirectUrl
-    return false
+export const allowPageAccessGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+):
+  | Observable<boolean | UrlTree>
+  | Promise<boolean | UrlTree>
+  | boolean
+  | UrlTree => {
+  const router = inject(Router);
+  if (environment.restrictedPages.includes(route.data['pageId'])) {
+    // location.href = environment.unauthorizedRedirectUrl
+    return false;
   }
   return true;
 };
--- src/app/services/interceptor/api.interceptor.ts
+++ src/app/services/interceptor/api.interceptor.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpErrorResponse,
+} from '@angular/common/http';
 import { Observable, throwError, fromEvent, merge, from } from 'rxjs';
 import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 import { Router } from '@angular/router';
@@ -12,18 +18,28 @@ import { environment } from 'src/environments/environment';
 })
 export class ApiInterceptor implements HttpInterceptor {
   private onlineStatus: boolean = true;
-  constructor(private router: Router,private toast:ToastService,private utilService:UtilService,private apiCallWithoutInteceptor:ApiWithoutInteceptor) {
+  constructor(
+    private router: Router,
+    private toast: ToastService,
+    private utilService: UtilService,
+    private apiCallWithoutInteceptor: ApiWithoutInteceptor
+  ) {
     const onlineEvent = fromEvent(window, 'online').pipe(map(() => true));
     const offlineEvent = fromEvent(window, 'offline').pipe(map(() => false));
-    merge(onlineEvent, offlineEvent).pipe(startWith(navigator.onLine)).subscribe(isOnline => {
-      this.onlineStatus = isOnline;
-      if (!this.onlineStatus) {
-        this.toast.presentToast('NETWORK_OFFLINE', 'danger');
-      }
-    });
-   }
+    merge(onlineEvent, offlineEvent)
+      .pipe(startWith(navigator.onLine))
+      .subscribe((isOnline) => {
+        this.onlineStatus = isOnline;
+        if (!this.onlineStatus) {
+          this.toast.presentToast('NETWORK_OFFLINE', 'danger');
+        }
+      });
+  }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     if (!this.onlineStatus) {
       return this.handleOfflineError();
     }
@@ -31,14 +47,19 @@ export class ApiInterceptor implements HttpInterceptor {
     return from(this.getToken()).pipe(
       switchMap((token) => {
         const authReq = this.addAuthHeader(req, token);
-        return next.handle(authReq).pipe(
-        catchError((error: HttpErrorResponse) => this.handleError(error))
-        );
+        return next
+          .handle(authReq)
+          .pipe(
+            catchError((error: HttpErrorResponse) => this.handleError(error))
+          );
       })
     );
   }
 
-  private addAuthHeader(req: HttpRequest<any>, token: string | null): HttpRequest<any> {
+  private addAuthHeader(
+    req: HttpRequest<any>,
+    token: string | null
+  ): HttpRequest<any> {
     if (!token) {
       return req;
     }
@@ -48,43 +69,48 @@ export class ApiInterceptor implements HttpInterceptor {
 
     if (this.isSpecialUrl(req.url)) {
       return req.clone({
-        setHeaders: extraHeaders ? { 'X-auth-token': `bearer ${token}`,...extraHeaders } : { 'X-auth-token': `bearer ${token}` }
+        setHeaders: extraHeaders
+          ? { 'X-auth-token': `bearer ${token}`, ...extraHeaders }
+          : { 'X-auth-token': `bearer ${token}` },
       });
-    } else if (req.headers.has("skipInterceptor")) {
+    } else if (req.headers.has('skipInterceptor')) {
       return req.clone({
         headers: req.headers.delete('skipInterceptor'),
       });
     } else {
-        return req.clone({
-          setHeaders: extraHeaders ? { 'X-auth-token': token, ...extraHeaders } : { 'X-auth-token': token }
-        });
-  }
+      return req.clone({
+        setHeaders: extraHeaders
+          ? { 'X-auth-token': token, ...extraHeaders }
+          : { 'X-auth-token': token },
+      });
+    }
   }
 
   private isSpecialUrl(url: string): boolean {
-    return url.includes('/logout') ||
-           url.includes('/user/update') ||
-           url.includes('/cloud-services/file/getSignedUrl') ||
-           url.includes('getDownloadableUrl');
+    return (
+      url.includes('/logout') ||
+      url.includes('/user/update') ||
+      url.includes('/cloud-services/file/getSignedUrl') ||
+      url.includes('getDownloadableUrl')
+    );
   }
 
   private handleError(error: HttpErrorResponse): Observable<never> {
     if (error?.status === 401) {
       localStorage.clear();
       this.utilService.clearDatabase();
-      location.href = environment.unauthorizedRedirectUrl
+      // location.href = environment.unauthorizedRedirectUrl
       return throwError(() => ({
         status: 401,
         error: { message: 'Your session has expired. Please log in again.' },
       }));
     }
     return throwError(error);
-
-    }
+  }
   private handleOfflineError(): Observable<any> {
-        return throwError(() => ({
-          error:{message: 'NETWORK_OFFLINE'}
-        }));
+    return throwError(() => ({
+      error: { message: 'NETWORK_OFFLINE' },
+    }));
   }
 
   async getToken(): Promise<string | null> {
@@ -92,8 +118,8 @@ export class ApiInterceptor implements HttpInterceptor {
     if (!token) {
       return null;
     }
-    if(environment.isAuthBypassed){
-      return token
+    if (environment.isAuthBypassed) {
+      return token;
     }
     const isValidToken = await this.utilService.validateToken(token);
     if (!isValidToken) {
